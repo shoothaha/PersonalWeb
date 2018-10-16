9aeb99bf066809fac76613c390ce43139574e5c4
Shaohua Ma <shoothaha@gmail.com>
2018-10-16 14:02:56 +1000

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>window.Laravel = {csrfToken: '{{ csrf_token() }}' }</script>
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div id="crud-app"></div>
+        <script src="{{ asset('js/app.js')}}"></script>
+    </body>
+</html>
